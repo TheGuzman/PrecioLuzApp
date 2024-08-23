aede6eaba1353be1e0d44dd59a2bcc59017b38dd
TheGuzman <guzmanmedrano.diego@gmail.com>
2024-08-23 10:51:31 +0200

    feat(initial-commit): add initial config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'precio-luz',
+  webDir: 'www',
+};
+
+export default config;
